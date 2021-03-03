d7540412050371465113220c546098c9685602d4
Danilo Monteiro <danilomonteiroo@gmail.com>
2021-03-03 15:15:50 -0300

    Script pulando linha
    Adicionamos um novo comando para pular linha

--- ScriptDeAcompanhamento/scriptAcompanhamento.js
+++ ScriptDeAcompanhamento/scriptAcompanhamento.js
@@ -1,6 +1,6 @@
 //var FORM_ID = '1UsmbeqAyXFf1uXpCTQvistEUX6phOTo7lDhwwz7i4ls';
 var SEL_STUDENT = "luiz.barbosa@zup.com.br";
-var SEND_MAIL = "danilo.lucena@zup.com.br, danilo.ribeiro@zup.com.br, alberto.tavares@zup.com.br";
+var SEND_MAIL = "danilo.lucena@zup.com.br, danilo.ribeiro@zup.com.br";
 var FORM_ID = ['1CTpEsKnUpqXUqLHzsFIx4V7kGe5F6U56830U4YowfAY','1vYFCuQC3g2UVVFYqgw_EwG9O-fkzXBKF0Atho_WZaGo','1AcS9CUapi5lydtL0C4TUVpEGG8FG1ivHlw4GoX0CjD0'];
 var arrayMensage= [ ];
 
@@ -23,23 +23,22 @@ function monitorRespostas() {
     if (SEL_STUDENT == formResponse.getRespondentEmail()){
       Logger.log("Aluno respondeu: %s", formResponse.getRespondentEmail())
 
-      arrayMensage.push("O estudante já respondeu o curso :"+form.getTitle() );
+      arrayMensage.push(form.getTitle() + " \n" );
 
-      // envia email
-          } 
-             
-          // remove os triggers para evitar múltiplos e-mails
-          //cancelTriggers();
-  
+           } 
+
 
           }  
-          if(arrayMensage.length > 0){
+         
+  } 
+  // envia email
+    if(arrayMensage.length > 0){
             MailApp.sendEmail(SEND_MAIL,
-             "Acompanhamento de estudante :"+SEL_STUDENT,
+             "Mensagem Automática: Acompanhamento de estudante :"+SEL_STUDENT,
              arrayMensage.toString() );
-  }  
+    }
  }
-}  
+ 
 
 function cancelTriggers(){
   //Logger.log('Current project has ' + ScriptApp.getProjectTriggers().length + ' triggers.');
